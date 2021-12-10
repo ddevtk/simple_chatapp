e56b8af39b1614034309f322cc65ad9640ef47ba
duongbui123 <buiducduong2342000@gmail.com>
2021-12-10 19:47:08 +0700

    fix ruby version and building app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_145429) do
+ActiveRecord::Schema.define(version: 2021_12_10_120718) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
